02c5fcb1f9c3a4f56dad7d6415b93b1dc702b8d9
dsszzi <anatoliipetliak@gmail.com>
2021-11-17 19:24:23 +0200

    HW

--- Lesson_3/HW/main.js
+++ Lesson_3/HW/main.js
@@ -114,12 +114,34 @@
 //     console.log(arr[i]);
 // }
 // - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 10; i++) {
+//     console.log( + i + '');
+//     document.write( + i + '');
+// }
 
-for (let i = 0; i < 10; i++) {
-    console.log()
-
-}
 // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i < 100; i++) {
+//     console.log( + i + '');
+//     document.write( + i + '');
+// }
 // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 100; i+=2) {
+//     console.log( + i + '');
+//     document.write( + i + '');
+// }
 // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
-// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// for (let i = 0; i < 100; i++) {
+//     (i % 2 === 0) ? console.log(+i + '') : console.log();
+//     document.write( + i + '');
+//     }
+//- Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+
+// for (let i = 0; i < 100; i++) {
+//     if (i % 2 === 1) {
+//         console.log(+i + ',');
+//         document.write(+i + ',');
+//     }
+// }
