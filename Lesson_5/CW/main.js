74377da49d079ba5093eded22991046761334fa7
Anatolii Petliak <anatoliipetliak@gmail.com>
2021-11-24 16:33:33 +0200

    CW

--- Lesson_5/CW/main.js
+++ Lesson_5/CW/main.js
@@ -1,5 +1,5 @@
 // - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-function min(a, b, c) {
+let min = (a, b, c) => {
     if (a < b && a < c) {
         console.log(a);
     } else if (b < a && b < c) {
@@ -11,12 +11,12 @@ function min(a, b, c) {
 
 min(3, 6, 9);
 min(8, 2, 0);
-min(28, 46, 278)
+min(28, 46, 278);
 
 
 // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
 
-function max(a, b, c) {
+let max = (a, b, c) => {
     if (a > b && a > c) {
         console.log(a);
     } else if (b > a && b > c) {
@@ -28,40 +28,39 @@ function max(a, b, c) {
 
 max(3, 6, 9);
 max(8, 2, 0);
-max(28, 46, 278)
+max(28, 46, 278);
 
 // - створити функцію яка повертає найбільше число з масиву
-function numbersOfPosts(arr) {
+let numbersOfPosts = (arr) => {
     let maxNumber = arr[0];
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > maxNumber) {
-            maxNumber = arr[i]
+            maxNumber = arr[i];
         }
     }
     console.log(maxNumber);
 }
 
-numbersOfPosts([1, 2, 3, 4, 5, 6])
+numbersOfPosts([1, 2, 3, 4, 5, 6]);
 
 
 // - створити функцію яка повертає найменьше число з масиву
-function numbersOfPosts2(arr) {
+let numbersOfPosts2 = (arr) => {
     let minNumber = arr[0];
-
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < minNumber) {
-            minNumber = arr[i]
+            minNumber = arr[i];
         }
     }
     console.log(minNumber);
 }
 
-numbersOfPosts2([1, 2, 3, 4, 5, 6])
+numbersOfPosts2([1, 2, 3, 4, 5, 6]);
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
-function arrSum(arr) {
+let arrSum = (arr) => {
     let arrElement = 0;
     for (let i = 0; i < arr.length; i++) {
         arrElement += arr[i];
@@ -73,7 +72,7 @@ arrSum([1, 2, 10]);
 
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
-function arrSum(arr) {
+let arrSum3 = (arr) => {
     let arrElement = 0;
     for (let i = 0; i < arr.length; i++) {
         arrElement += arr[i];
@@ -81,10 +80,10 @@ function arrSum(arr) {
     console.log(arrElement / arr.length);
 }
 
-arrSum([10, 10, 10]);
+arrSum3([10, 10, 10]);
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-function arrSum2(arguments) {
+let arrSum2 = (arguments) => {
     let min = arguments[0];
     let max = arguments[0];
     for (let item of arguments) {
@@ -105,7 +104,7 @@ console.log(x);
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
 let numberArr = [];
-function randomArr(min, max) {
+let randomArr = (min, max) => {
     for (let i = 0; i < max; i++) {
         let randomNumber = Math.floor(Math.random() * (max - min) + min);
         numberArr.push(randomNumber);
@@ -118,6 +117,7 @@ randomArr(0, 100);
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
 
 let numberArr2 = [];
+
 function randomArr2(min, limit) {
     for (let i = 0; i < limit; i++) {
         let randomNumber2 = Math.floor(Math.random() * (limit - min) + min);
@@ -126,15 +126,16 @@ function randomArr2(min, limit) {
 
     console.log(numberArr2);
 }
+
 randomArr2(0, 100);
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
 
 let someArr = [];
-function reverseArr (arr) {
+let reverseArr = (arr) => {
     console.log(arr);
-    for (let i = arr.length -1; i >= 0; i--) {
+    for (let i = arr.length - 1; i >= 0; i--) {
         someArr.push(arr[i]);
     }
     console.log(someArr);
 }
-reverseArr([1, 2, 3]);
+reverseArr([1, 5, 3]);
