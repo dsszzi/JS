54c593f352a03a86b99216d087ce1a5f5314e546
dsszzi <anatoliipetliak@gmail.com>
2021-11-26 00:40:56 +0200

    email, bag

---
+++ Lesson_6/Additional/main.js
@@ -0,0 +1,149 @@
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
+
+
+// let arrHunter = (arr2) => {
+//     console.log(arr2.match(/.{1,3}/g));
+// }
+// arrHunter('наслаждение');
+//
+// //або так
+//
+// let arrHunter2 = (str, n) => {
+//     let res = [];
+//     while (str.length) {
+//         res.push(str.substring(0, n));
+//         str = str.substring(n);
+//     }
+//     return res;
+// }
+//
+// console.log(arrHunter2('наслаждение', 3));
+
+// - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
+// Протестувати на значеннях
+// someemail@gmail.com
+// someeMAIL@gmail.com
+// someeMAIL@i.ua
+// some.email@gmail.com
+//
+// Примітка
+// Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
+
+
+let emailValidate = (email) => {
+    let testEmail = email.toLowerCase();
+    let validateArr = [];
+    let symbol = testEmail.split('');
+    validateArr.push(symbol);
+    console.log(validateArr[0]);
+    for (let i = 0; i < validateArr[0].length; i++) {
+        const symbolElement = validateArr[0][i];
+        if (symbolElement === '@') {
+            return true;
+        } else if (symbolElement === '.') {
+            return true;
+        } else {
+            return false
+        }
+
+
+
+
+
+
+            // if (symbolElement === '.') {
+            //     if (symbolElement.indexOf('@')) {
+            //         if (symbolElement.indexOf('@') !== 0) {
+            //             if (symbolElement.indexOf('.') + 2 > symbolElement.indexOf('@')) {
+            //                 console.log('Valid');
+            //             }
+            //         }
+            //     }
+            // }
+        }
+
+
+}
+emailValidate('xafcdsfcsd@gmail.com');
+
+// - є масивlet coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+//
+// відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+//
+// - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+//     let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+//
+// - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
+//     let str = "Сила тяжести приложена к центру масс тела";
+// document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
