e0e037093bf2dc1efe677b47d46ab195774879c5
dsszzi <anatoliipetliak@gmail.com>
2021-11-30 20:44:08 +0200

    additional 7

--- Lesson_7/Additional/main.js
+++ Lesson_7/Additional/main.js
@@ -1,54 +1,23 @@
-// Skip to content
-// Search or jump to…
-// Pull requests
-// Issues
-// Marketplace
-// Explore
-//
-// @dsszzi
-// GrayHead
-// /
-// js_demos
-// Public
-// 8
-// 39125
-// Code
-// Issues
-// Pull requests
-// 1
-// Actions
-// Projects
-// Wiki
-// Security
-// Insights
-// js_demos/js/preview/lesson7/lesson7_tasks/additional/additional.txt
-// @GrayHead
-// GrayHead - поменял план
-// …
-// Latest commit bd5ac83 20 days ago
-// History
-// 1 contributor
-// 60 lines (54 sloc)  1.97 KB
-//
 //
 // - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
 // Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
 //
 // {
 //     id: 1,
-//         name: 'Leanne Graham',
+//     name: 'Leanne Graham',
 //     username: 'Bret',
 //     email: 'Sincere@april.biz',
 //     address: {
-//     street: 'Kulas Light',
+//         street: 'Kulas Light',
 //         suite: 'Apt. 556',
 //         city: 'Gwenborough',
 //         zipcode: '92998-3874',
-//         geo: {
-//         lat: '-37.3159',
-//             lng: '81.1496'
-//     }
-// },
+//              geo: {
+//                  lat: '-37.3159',
+//                  lng: '81.1496'
+//              }
+//      },
+// {
 //     phone: '1-770-736-8031 x56442',
 //         website: 'hildegard.org',
 //     company: {
@@ -57,8 +26,39 @@
 //         bs: 'harness real-time e-markets'
 // }
 // }
-//
-//
+
+
+class position {
+    constructor(lat, lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
+
+class clientAddress {
+    constructor(street, suite, city, zipcode, position) {
+        this.street = street;
+        this.suite = suite;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.position = position;
+    }
+}
+
+class createClient {
+    constructor(id, name, username, email, adress) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.address = adress;
+    }
+}
+
+let firstClient = new createClient(1, 'Leanne Graham', 'Bret', 'Sincere@april.biz', new clientAddress('Kulas Light', 'Apt. 556', 'Gwenborough', '92998-3874', new position('-37.3159', '81.1496')));
+console.log(firstClient);
+
+
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
 // Поля :
 //     -назва тегу ()
@@ -81,25 +81,40 @@
 // Приклад результуючого об'єкту
 // {
 //     titleOfTag: 'area',
-//         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
+//     action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
 //     attrs: [
-//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
-//     {/*some props and values*/},
-//     {/*...*/},
-//     {/*...*/},
-// ]
+//              {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
+//              {/*some props and values*/},
+//              {/*...*/},
+//              {/*...*/},
+//             ]
 //
 // }
-// © 2021 GitHub, Inc.
-//     Terms
-// Privacy
-// Security
-// Status
-// Docs
-// Contact GitHub
-// Pricing
-// API
-// Training
-// Blog
-// About
-// Loading complete
+
+class attrs {
+    constructor(titleOfAttr, props, values) {
+        this.titleOfAttr = titleOfAttr;
+        this.props = props;
+        this.values = values;
+    }
+}
+
+
+class htmlBook {
+    constructor(title, action, attrs) {
+        this.title = title;
+        this.acton = action;
+        this.attrs = attrs;
+    }
+}
+
+let tag = new htmlBook(
+    '232',
+    'njhangsjgvcdfg',
+    [
+        new attrs('a', 'Активация ссылки с помощью комбинации клавиш.', 'svgfdfg'),
+        new attrs('div', 'vsfgvdfg', 'Отношения между ссылаемым и текущим документами.'),
+
+    ]
+);
+console.log(tag)
